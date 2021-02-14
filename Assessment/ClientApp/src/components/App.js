8ab5570b964d0dd3f05f845bcadb449a35cbe560
Audry Wolters <audrywolters@gmail.com>
2021-02-13 23:25:43 -0600

    pairing down nav

--- Assessment/ClientApp/src/components/App.js
+++ Assessment/ClientApp/src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router';
+import { HashRouter as Router, Route, Link } from "react-router-dom";
 import { Layout } from './Layout';
 import { Home } from './Home';
 import { FetchData } from './FetchData';
@@ -9,17 +9,25 @@ import { SideView } from './SideView';
 import '../custom.css';
 
 export default class App extends Component {
-  //static displayName = App.name;
 
-  render () {
-    return (
-      <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data' component={FetchData} />
-        <Route path='/itemList' component={ItemList} />
-        <Route path='/sideView' component={SideView} />
-      </Layout>
-    );
-  }
+    render() {
+        return (
+            <Router>
+                <nav>
+                    <Link to="/">Home</Link>
+                    <Link to="/counter">Counter</Link>
+                    <Link to="/fetch-data">Fetch</Link>
+                    <Link to="/itemList">Item List</Link>
+                    <Link to="/sideView">Side View</Link>
+                </nav>
+                <main>
+                    <Route exact path="/" component={Home} />
+                    <Route path='/counter' component={Counter} />
+                    <Route path='/fetch-data' component={FetchData} />
+                    <Route path='/itemList' component={ItemList} />
+                    <Route path='/sideView' component={SideView} />
+                </main>
+            </Router>
+        );
+    }
 }
--- Assessment/Controllers/WeatherForecastController.cs
+++ Assessment/Controllers/WeatherForecastController.cs
@@ -17,12 +17,6 @@ namespace Assessment.Controllers
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
-        private readonly ILogger<WeatherForecastController> _logger;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
-            _logger = logger;
-        }
 
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
