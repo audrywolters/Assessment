05689a599b9597620af1b4ada0dfb68fea7b7c2f
Audry Wolters <audrywolters@gmail.com>
2021-02-16 12:33:40 -0600

    checkbox is working

--- Assessment/ClientApp/src/components/EmployeeList.js
+++ Assessment/ClientApp/src/components/EmployeeList.js
@@ -16,16 +16,29 @@ export class EmployeeList extends Component {
         this.getEmployeeList();
     }
 
-    handleInputChange = (evt) => {
+    handleCheckboxChange = (evt) => {
         // make a copy of employee so state doesn't get upset about immutablity
-        let newEmployee = this.state.employee;
+        let copyOfEmployee = this.state.employee;
 
-        // use <input name="foo" as variable so this method works for each input
-        newEmployee[evt.target.name] = evt.target.value;
+        // this is different because instead of "", we need ".checked"
+        copyOfEmployee[evt.target.name] = evt.target.checked;
 
         // set that state!
         this.setState({
-            employee: newEmployee
+            employee: copyOfEmployee
+        })
+    }
+
+    handleInputChange = (evt) => {
+        // make a copy of employee so state doesn't get upset about immutablity
+        let copyOfEmployee = this.state.employee;
+
+        // use <input name="foo" as variable so this method works for each input
+        copyOfEmployee[evt.target.name] = evt.target.value;
+
+        // set that state!
+        this.setState({
+            employee: copyOfEmployee
         })
 
         // AUDRY - it would be nice if the changes didn't update in listing
@@ -86,19 +99,20 @@ export class EmployeeList extends Component {
                     </div>
 
                     <ul>
-                        <li> <label>Name:</label>        <input name="name"          value={this.state.employee.name}          onChange={this.handleInputChange}  type="text" /> </li>
-                        <li> <label>Title:</label>       <input name="title"         value={this.state.employee.title}         onChange={this.handleInputChange}  type="text" /> </li>
-                        <li> <label>Direct:</label>      <input name="phoneDirect"   value={this.state.employee.phoneDirect}   onChange={this.handleInputChange}  type="text" /> </li>
-                        <li> <label>Cell:</label>        <input name="phoneCell"     value={this.state.employee.phoneCell}     onChange={this.handleInputChange}  type="text" /> </li>
-                        <li> <label>Home:</label>        <input name="phoneHome"     value={this.state.employee.phoneHome}     onChange={this.handleInputChange}  type="text" /> </li>
-                        <li> <label>Fax:</label>         <input name="faxDefault"    value={this.state.employee.faxDefault}    onChange={this.handleInputChange}  type="text" /> </li>
-                        <li> <label>Home Fax:</label>    <input name="faxHome"       value={this.state.employee.faxHome}       onChange={this.handleInputChange}  type="text" /> </li>
-                        <li> <label>Email:</label>       <input name="emailDefault"  value={this.state.employee.emailDefault}  onChange={this.handleInputChange}  type="text" /> </li>
-                        <li> <label>Email Home:</label>  <input name="emailHome"     value={this.state.employee.emailHome}     onChange={this.handleInputChange}  type="text" /> </li>
-                        { /*  checkbox will need a special event handler */ }
-                        <li> <label>Is Active:</label>   <input name="isActive" type="checkbox" checked={this.state.employee.isActive} />                                    </li>
+                        <li> <label>Name:</label>        <input name="name"          value={this.state.employee.name}          onChange={this.handleInputChange}    type="text" />    </li>
+                        <li> <label>Title:</label>       <input name="title"         value={this.state.employee.title}         onChange={this.handleInputChange}    type="text" />    </li>
+                        <li> <label>Direct:</label>      <input name="phoneDirect"   value={this.state.employee.phoneDirect}   onChange={this.handleInputChange}    type="text" />    </li>
+                        <li> <label>Cell:</label>        <input name="phoneCell"     value={this.state.employee.phoneCell}     onChange={this.handleInputChange}    type="text" />    </li>
+                        <li> <label>Home:</label>        <input name="phoneHome"     value={this.state.employee.phoneHome}     onChange={this.handleInputChange}    type="text" />    </li>
+                        <li> <label>Fax:</label>         <input name="faxDefault"    value={this.state.employee.faxDefault}    onChange={this.handleInputChange}    type="text" />    </li>
+                        <li> <label>Home Fax:</label>    <input name="faxHome"       value={this.state.employee.faxHome}       onChange={this.handleInputChange}    type="text" />    </li>
+                        <li> <label>Email:</label>       <input name="emailDefault"  value={this.state.employee.emailDefault}  onChange={this.handleInputChange}    type="text" />    </li>
+                        <li> <label>Email Home:</label>  <input name="emailHome"     value={this.state.employee.emailHome}     onChange={this.handleInputChange}    type="text" />    </li>
+                        <li> <label>Is Active:</label>   <input name="isActive"    checked={this.state.employee.isActive}      onChange={this.handleCheckboxChange} type="checkbox"/> </li>
                     </ul>
-                </div>   
+                    {JSON.stringify(this.state.employee)}
+                </div>
+
             </>
         );
     }
